8f2f0e94b531c835cde1c3191079939b8e832862
shekhaa <shekharsinghrana.1001@gmail.com>
2022-02-09 20:45:53 +0530

    added static

--- mac/urls.py
+++ mac/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/',admin.site.urls,name="admin"),
     path('',views.home,name="home"),
     path('about',views.about,name='about'),
     path('service',views.service,name="service"),
+    
     path('shop/', include('shop.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
